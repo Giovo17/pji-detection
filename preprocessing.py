96eb785ba4bc844e81659400a33e33349ed7c399
Giovanni Spadaro <giovannispada17.gs@tutanota.com>
2024-02-25 16:28:37 +0100

    fixed resized images output path

--- preprocessing.py
+++ preprocessing.py
@@ -94,31 +94,47 @@ def resize_patients(import_path: str, export_path: str, desired_size: Tuple[int]
     export_path = os.path.join(export_path, technique + "_" + str(desired_size[0])+'x'+str(desired_size[1])) # Adjust the export path
     os.makedirs(export_path, exist_ok=True)
 
+    # Create infected and normal patients folders
+    os.makedirs(os.path.join(export_path, "Pazienti_Asettici"), exist_ok=True)
+    os.makedirs(os.path.join(export_path, "Pazienti_Settici"), exist_ok=True)
+    
 
     import_dir = import_path.split("/")[-1]
     print(f"IMPORT: {import_dir}")
     export_dir = export_path.split("/")[-2] + "/" + export_path.split("/")[-1]
     print(f"EXPORT: {export_dir}")
 
-    # Cycle through all subfolder and recreate them into export_path
-    for root, dirs, files in os.walk(import_path, topdown=False):
-        for name in dirs:
-            os.makedirs(os.path.join(root, name).replace(import_dir, export_dir), exist_ok=True)
 
     # Cycle through all files and convert them to png and save to output directory
     for root, dirs, files in os.walk(import_path, topdown=False):
         for name in files:            
             file_path = os.path.join(root, name)
             try:
-                image = Image.open(file_path)
+                image = Image.open(file_path).convert("L")
             except UnidentifiedImageError as e:
                 print(f"cannot identify image file {file_path}")
                 print(e)
                 continue
 
             resized_image = image.resize(desired_size)
+            
+            resized_image.convert("RGB") # Convert to RGB cause almost every CNN take as input 3-channels images
 
-            output_file_path = file_path.replace(import_dir, export_dir).split(".")[0] + ".png"
+            # Output filepath
+            splitted_file_path = file_path.split("/")
+            img_id = splitted_file_path[-1].split("Patient")[-1][2:]
+            patient = splitted_file_path[-1].split("Patient")[-1][0:2]
+            splitted_file_path.pop(-1)
+            while(len(splitted_file_path) > 0):
+                if "Patient" in splitted_file_path[-1]:
+                    splitted_file_path.pop()
+                    break
+                img_id = splitted_file_path[-1] + "_" + img_id
+                splitted_file_path.pop()
+            
+            base_output_path = "/".join(splitted_file_path).replace(import_dir, export_dir)
+            output_file_path = os.path.join(base_output_path, "Patient" + patient + "_" + img_id)
+                
             resized_image.save(output_file_path)
 
 
@@ -130,7 +146,7 @@ def main():
     
     print("Init preprocessing")
 
-    preprocess_patients(dicom_data_path, png_export_data_path)
+    #preprocess_patients(dicom_data_path, png_export_data_path)
 
     resize_patients(png_export_data_path, png_resized_data_path, (227,227))
     
