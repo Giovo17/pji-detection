e6625f8d04a57d976a135998d34df333f3707030
Gabriele Tuccio <tuccio.gabriele@yahoo.it>
2024-02-27 16:02:09 +0100

    update

--- benchmarks.m
+++ benchmarks.m
@@ -34,7 +34,30 @@ function [net, info] = resnet50_finetuning(train_data, opts)
 end
 
 
+function [net, info] = squeeznet_finetuning(train_data, opts)
 
+
+    numClasses = numel(categories(train_data.Labels));
+
+
+    net = squeeznet;
+    %deepNetworkDesigner(net)
+    inputSize = net.Layers(1).InputSize
+    lgraph = layerGraph(net); 
+
+
+    % Specify new layers for classification based on number of unique classes
+    new_convolution2dLayer =  convolution2dLayer([1 1],2,"new_conv10")
+    lgraph = replaceLayer(lgraph,"conv10",new_convolution2dLayer);
+
+    newClassLayer = classificationLayer('Name','new_classoutput');
+    lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',newClassLayer);
+
+
+    % Model finetuning
+    [net, info] = trainNetwork(train_data, lgraph, opts);
+
+end
 
 
 % Data import
@@ -45,11 +68,12 @@ imds = imageDatastore(dataFolder, 'FileExtensions', exts, 'IncludeSubfolders', t
 
 
 % Data augmentation
-pixelRange = [-30 30];
 imageAugmenter = imageDataAugmenter( ...
     'RandXReflection',true, ...
-    'RandXTranslation',pixelRange, ...
-    'RandYTranslation',pixelRange);
+    'RandYReflection',true, ...
+    'RandRotation',[0 360], ...
+    'RandXTranslation',[-30 30], ...
+    'RandYTranslation',[-30 30]);
 
 augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain,'DataAugmentation',imageAugmenter);
 augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
@@ -60,13 +84,15 @@ opts = trainingOptions('adam', ...
                        'MiniBatchSize', 32, ...
                        'MaxEpochs', 1, ...
                        'InitialLearnRate', 1e-4, ...
+                       'L2Regularization', 0.0005, ...
                        'Shuffle','every-epoch', ...
                        'ValidationData', augimdsValidation, ...
-                       'ValidationFrequency', 3, ...
+                       'ValidationFrequency', 150, ...
                        'Plots', 'training-progress', ...
                        'Verbose', false);
 
 
-resnet50_finetuning(augimdsTrain, opts)
+%resnet50_finetuning(augimdsTrain, opts)
+squeeznet_finetuning(augimdsTrain, opts)
 
 
