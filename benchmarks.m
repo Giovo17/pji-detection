c43a43acfe394f6d8f573ce02c3b4dd4fb1554a0
Giovanni Spadaro <giovannispada17.gs@tutanota.com>
2024-02-27 15:50:00 +0100

    update benchmark script

--- benchmarks.m
+++ benchmarks.m
@@ -3,32 +3,45 @@ clear;
 close all;
 
 
-% Resnet50 finetuning
 
+
+function [net, info] = resnet50_finetuning(train_data, opts)
+
+
+    numClasses = numel(categories(train_data.Labels));
+
+
+    net = resnet50;
+    %deepNetworkDesigner(net)
+    inputSize = net.Layers(1).InputSize
+    lgraph = layerGraph(net); 
+
+
+    % Specify new layers for classification based on number of unique classes
+    newfc = fullyConnectedLayer(numClasses, ...
+                                'Name','new_fc', ...
+                                'WeightLearnRateFactor',10, ...
+                                'BiasLearnRateFactor',10);
+    lgraph = replaceLayer(lgraph,'fc1000',newfc);
+
+    newClassLayer = classificationLayer('Name','new_classoutput');
+    lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',newClassLayer);
+
+
+    % Model finetuning
+    [net, info] = trainNetwork(train_data, lgraph, opts);
+
+end
+
+
+
+
+
+% Data import
 dataFolder = '~/Desktop/rescale_224x224/';
 exts = {'.png'}
 imds = imageDatastore(dataFolder, 'FileExtensions', exts, 'IncludeSubfolders', true, 'LabelSource','foldernames');
 [imdsTrain, imdsValidation] = splitEachLabel(imds,0.7,'randomized');
-
-
-numClasses = numel(categories(imdsTrain.Labels));
-
-
-net = resnet50;
-%deepNetworkDesigner(net)
-inputSize = net.Layers(1).InputSize
-lgraph = layerGraph(net); 
-
-
-% Specify new layers for classification based on number of unique classes
-newfc = fullyConnectedLayer(numClasses, ...
-    'Name','new_fc', ...
-    'WeightLearnRateFactor',10, ...
-    'BiasLearnRateFactor',10);
-lgraph = replaceLayer(lgraph,'fc1000',newfc);
-
-newClassLayer = classificationLayer('Name','new_classoutput');
-lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',newClassLayer);
 
 
 % Data augmentation
@@ -37,29 +50,23 @@ imageAugmenter = imageDataAugmenter( ...
     'RandXReflection',true, ...
     'RandXTranslation',pixelRange, ...
     'RandYTranslation',pixelRange);
-augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
-    'DataAugmentation',imageAugmenter);
 
-
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain,'DataAugmentation',imageAugmenter);
 augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
-
 
 
 % Training hyperparameters
 opts = trainingOptions('adam', ...
                        'MiniBatchSize', 32, ...
-                       'MaxEpochs', 5, ...
-                       'InitialLearnRate',1e-4, ...
+                       'MaxEpochs', 1, ...
+                       'InitialLearnRate', 1e-4, ...
                        'Shuffle','every-epoch', ...
                        'ValidationData', augimdsValidation, ...
-                       'ValidationFrequency',3, ...
+                       'ValidationFrequency', 3, ...
                        'Plots', 'training-progress', ...
                        'Verbose', false);
 
 
-% Model finetuning
-[net, info] = trainNetwork(augimdsTrain, lgraph, opts);
-
-
+resnet50_finetuning(augimdsTrain, opts)
 
 
