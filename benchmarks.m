0e2ed7dff5b3979652ca07b0822c3df8c183207f
Giovanni Spadaro <19341666+Giovo17@users.noreply.github.com>
2024-02-29 00:04:25 +0100

    code cleaning

--- benchmarks.m
+++ benchmarks.m
@@ -2,6 +2,7 @@ clc;
 clear;
 close all;
 
+
 % Data import
 %dataFolder = '~/Desktop/rescale_224x224/'; %resnet18, resnet50,  googlenet
 %dataFolder = '~/Desktop/rescale_227x227/'; % squeezenet
@@ -29,20 +30,19 @@ darknet19_finetuning(imdsTrain, imdsValidation, imageAugmenter)
 
 
 
-
 function [net, info] = squeezenet_finetuning(train_data, val_data, imageAugmenter)
 
-
     numClasses = numel(categories(train_data.Labels));
-
 
     net = squeezenet;
     inputSize = net.Layers(1).InputSize
     lgraph = layerGraph(net); 
 
+
     % Data Augmentation
     augimdsTrain = augmentedImageDatastore(inputSize(1:2),train_data,'DataAugmentation',imageAugmenter);
     augimdsValidation = augmentedImageDatastore(inputSize(1:2),val_data);
+
 
     % Specify new layers for classification based on number of unique classes
     new_convolution2dLayer =  convolution2dLayer(1,2,'Name',"new_conv10",'Stride',1,'Padding',0);
@@ -50,6 +50,7 @@ function [net, info] = squeezenet_finetuning(train_data, val_data, imageAugmente
 
     newClassLayer = classificationLayer('Name','new_classoutput');
     lgraph = replaceLayer(lgraph,'ClassificationLayer_predictions',newClassLayer);
+
 
     % Training hyperparameters
     opts = trainingOptions('adam', ...
@@ -67,34 +68,32 @@ function [net, info] = squeezenet_finetuning(train_data, val_data, imageAugmente
     % Model finetuning
     [net, info] = trainNetwork(train_data, lgraph, opts);
 
-
 end
 
 
 
 function [net, info] = googlenet_finetuning(train_data, val_data, imageAugmenter)
 
-
     numClasses = numel(categories(train_data.Labels));
-
 
     net = googlenet;
     inputSize = net.Layers(1).InputSize
     lgraph = layerGraph(net); 
 
+
     % Data Augmentation
     augimdsTrain = augmentedImageDatastore(inputSize(1:2),train_data,'DataAugmentation',imageAugmenter);
     augimdsValidation = augmentedImageDatastore(inputSize(1:2),val_data);
 
+
     % Specify new layers for classification based on number of unique classes
-
-
     newfc = fullyConnectedLayer(2,"Name","loss3-classifier","BiasLearnRateFactor",2);
     lgraph = replaceLayer(lgraph,"loss3-classifier",newfc);
 
     newClassLayer = classificationLayer('Name','new_classoutput');
     lgraph = replaceLayer(lgraph,'output',newClassLayer);
     
+
     % Training hyperparameters
      opts = trainingOptions('adam', ...
                            'MiniBatchSize', 32, ...
@@ -107,6 +106,7 @@ function [net, info] = googlenet_finetuning(train_data, val_data, imageAugmenter
                            'Plots', 'training-progress', ...
                            'Verbose', false);
 
+
     % Model finetuning
     [net, info] = trainNetwork(train_data, lgraph, opts);
 
@@ -116,28 +116,26 @@ end
 
 function [net, info] = resnet18_finetuning(train_data, val_data, imageAugmenter)
 
-
     numClasses = numel(categories(train_data.Labels));
-
 
     net = resnet18;
     inputSize = net.Layers(1).InputSize
     lgraph = layerGraph(net); 
 
+
     % Data Augmentation
     augimdsTrain = augmentedImageDatastore(inputSize(1:2),train_data,'DataAugmentation',imageAugmenter);
     augimdsValidation = augmentedImageDatastore(inputSize(1:2),val_data);
 
+
     % Specify new layers for classification based on number of unique classes
-    newfc = fullyConnectedLayer(numClasses, ...
-                                'Name','new_fc', ...
-                                'WeightLearnRateFactor',10, ...
-                                'BiasLearnRateFactor',10);
+    newfc = fullyConnectedLayer(numClasses, 'Name','new_fc','WeightLearnRateFactor',10,'BiasLearnRateFactor',10);
     lgraph = replaceLayer(lgraph,'fc1000',newfc);
 
     newClassLayer = classificationLayer('Name','new_classoutput');
     lgraph = replaceLayer(lgraph,'ClassificationLayer_predictions',newClassLayer);
     
+
     % Training hyperparameters
      opts = trainingOptions('adam', ...
                            'MiniBatchSize', 32, ...
@@ -150,6 +148,7 @@ function [net, info] = resnet18_finetuning(train_data, val_data, imageAugmenter)
                            'Plots', 'training-progress', ...
                            'Verbose', false);
 
+
     % Model finetuning
     [net, info] = trainNetwork(train_data, lgraph, opts);
 
@@ -159,28 +158,26 @@ end
 
 function [net, info] = resnet50_finetuning(train_data, val_data, imageAugmenter)
 
-
     numClasses = numel(categories(train_data.Labels));
-
 
     net = resnet50;
     inputSize = net.Layers(1).InputSize
     lgraph = layerGraph(net); 
 
+
     % Data Augmentation
     augimdsTrain = augmentedImageDatastore(inputSize(1:2),train_data,'DataAugmentation',imageAugmenter);
     augimdsValidation = augmentedImageDatastore(inputSize(1:2),val_data);
 
+
     % Specify new layers for classification based on number of unique classes
-    newfc = fullyConnectedLayer(numClasses, ...
-                                'Name','new_fc', ...
-                                'WeightLearnRateFactor',10, ...
-                                'BiasLearnRateFactor',10);
+    newfc = fullyConnectedLayer(numClasses,'Name','new_fc','WeightLearnRateFactor',10,'BiasLearnRateFactor',10);
     lgraph = replaceLayer(lgraph,'fc1000',newfc);
 
     newClassLayer = classificationLayer('Name','new_classoutput');
     lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',newClassLayer);
     
+
     % Training hyperparameters
      opts = trainingOptions('adam', ...
                            'MiniBatchSize', 32, ...
@@ -192,6 +189,7 @@ function [net, info] = resnet50_finetuning(train_data, val_data, imageAugmenter)
                            'ValidationFrequency', 50, ...
                            'Plots', 'training-progress', ...
                            'Verbose', false);
+
 
     % Model finetuning
     [net, info] = trainNetwork(train_data, lgraph, opts);
@@ -202,27 +200,26 @@ end
 
 function [net, info] = darknet19_finetuning(train_data, val_data, imageAugmenter)
 
-
     numClasses = numel(categories(train_data.Labels));
-
 
     net = darknet19;
     inputSize = net.Layers(1).InputSize
     lgraph = layerGraph(net); 
 
+
     % Data Augmentation
     augimdsTrain = augmentedImageDatastore(inputSize(1:2),train_data,'DataAugmentation',imageAugmenter);
     augimdsValidation = augmentedImageDatastore(inputSize(1:2),val_data);
 
+
     % Specify new layers for classification based on number of unique classes
-
-
-    new_convolution2dLayer =  convolution2dLayer(1,2,'Name',"new_conv19",'Stride',1,'Padding',0);
+    new_convolution2dLayer = convolution2dLayer(1,2,'Name',"new_conv19",'Stride',1,'Padding',0);
     lgraph = replaceLayer(lgraph,"conv19",new_convolution2dLayer);
 
     newClassLayer = classificationLayer('Name','new_classoutput');
     lgraph = replaceLayer(lgraph,'output',newClassLayer);
     
+
     % Training hyperparameters
      opts = trainingOptions('adam', ...
                            'MiniBatchSize', 32, ...
@@ -235,10 +232,10 @@ function [net, info] = darknet19_finetuning(train_data, val_data, imageAugmenter
                            'Plots', 'training-progress', ...
                            'Verbose', false);
 
+
     % Model finetuning
     [net, info] = trainNetwork(train_data, lgraph, opts);
 
 end
-
 
 
