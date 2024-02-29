3c30055499cf8193b8a5cc9e2dac189f1bde8485
Giovanni Spadaro <19341666+Giovo17@users.noreply.github.com>
2024-02-29 14:25:48 +0100

    fixes

--- extract_metrics.py
+++ extract_metrics.py
@@ -10,50 +10,28 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 labels = ['septic', 'aseptic']
 
 
-def extract_clf_metrics(y_true: List[str], y_predicted: List[str], output_path: str, model_name: str):
-
+def extract_clf_metrics(y_true: List[str], y_pred: List[str], output_path: str, model_name: str):
+    """Extract classification metrics."""
 
     print(f"Model: {model_name}")
 
     # Confusion matrix
-    cm = confusion_matrix(y_true=y_true, y_pred=y_predicted, labels=labels)
+    cm = confusion_matrix(y_true=y_true, y_pred=y_pred, labels=labels)
     cf_plot = sns.heatmap(cm, annot=True, fmt="d", cmap='Blues', xticklabels=labels, yticklabels=labels)
     cf_plot.set_xlabel("Predicted")
     cf_plot.set_ylabel("True")
-    plt.savefig(os.path.join(output_path, model_name, "cm.png"))
+    plt.savefig(os.path.join(output_path, model_name + "_cm.png"))
 
-    
-    # Calculate average accuracy and overall f1-score
-    accuracy = accuracy_score(y_true, y_predicted)
 
-    f1_micro = f1_score(y_true, y_predicted, average='micro')
-    f1_macro = f1_score(y_true, y_predicted, average='macro')
-    f1_weighted = f1_score(y_true, y_predicted, average='weighted')
+    accuracy = accuracy_score(y_true=y_true, y_pred=y_pred)
+    precision = precision_score(y_true=y_true, y_pred=y_pred)
+    recall = recall_score(y_true=y_true, y_pred=y_pred)
+    f1 = f1_score(y_true=y_true, y_pred=y_pred)
 
     print("Accuracy: " + str(round(accuracy, 4)))
-    print("F1 score averaged micro: " + str(round(f1_micro, 4)))
-    print("F1 score averaged macro: " + str(round(f1_macro, 4)))
-    print("F1 score averaged weighted: " + str(round(f1_weighted, 4)))
-
-
-    # Metrics of septic class
-    recall_class_0 = recall_score(y_true, y_predicted, labels=['septic'])
-    precision_class_0 = precision_score(y_true, y_predicted, labels=['septic'])
-    f1_class_0 = f1_score(y_true, y_predicted, labels=['septic'])
-
-    print("Recall for class septic: " + str(round(recall_class_0, 4)))
-    print("Precision for class septic: " + str(round(precision_class_0, 4)))
-    print("F1-score for class septic: " + str(round(f1_class_0, 4)))
-
-
-    # Metrics of aseptic class
-    recall_class_1 = recall_score(y_true, y_predicted, labels=['aseptic'])
-    precision_class_1 = precision_score(y_true, y_predicted, labels=['aseptic'])
-    f1_class_1 = f1_score(y_true, y_predicted, labels=['aseptic'])
-
-    print("Recall for class aseptic: " + str(round(recall_class_1, 4)))
-    print("Precision for class aseptic: " + str(round(precision_class_1, 4)))
-    print("F1-score for class aseptic: " + str(round(f1_class_1, 4)))
+    print("Precision: " + str(round(precision, 4)))
+    print("Recall: " + str(round(recall, 4)))
+    print("F1-score: " + str(round(f1, 4)))
 
 
     print("-------------------------------------------------------------------------------")
@@ -62,25 +40,22 @@ def extract_clf_metrics(y_true: List[str], y_predicted: List[str], output_path: 
 def main():
 
     models = ["squeezenet", "googlenet", "resnet18", "resnet50", "darknet19"]
+    labels_mapping = {"Pazienti_Settici": "septic", 
+                      "Pazienti_Asettici": "aseptic"}
 
     for model in models:
 
-        results_path = os.path.join(os.getcwd(), "data", "results", "squeezenet")
+        results_path = os.path.join(os.getcwd(), "data", "results", model)
 
-        y_true_df = pd.read_csv(os.path.join(results_path, "squeezenet_ytrue.csv"), header=None)
+        y_true_df = pd.read_csv(os.path.join(results_path, model + "_ytrue.csv"), header=None)
         y_true = y_true_df.loc[:,0].to_list()
-        y_pred_df = pd.read_csv(os.path.join(results_path, "squeezenet_ypred.csv"), header=None)
+        y_true = list(map(labels_mapping.get, map(str, y_true)))
+        y_pred_df = pd.read_csv(os.path.join(results_path, model + "_ypred.csv"), header=None)
         y_pred = y_pred_df.loc[:,0].to_list()
+        y_pred = list(map(labels_mapping.get, map(str, y_pred)))
+        
 
-        extract_clf_metrics(y_true, y_pred, results_path, "squeezenet")
-
-
-
-    
-
-
-
-
+        extract_clf_metrics(y_true, y_pred, results_path, model)
 
 
 
